98d840f8f28b666cf6bdc8b4a18d439650f68cc1
Rishichandra Wawhal <rishi@hasura.io>
2021-09-07 14:27:38 +0530

    handle event based on pr app name

--- scripts/deploy-preview-app.js
+++ scripts/deploy-preview-app.js
@@ -5,45 +5,61 @@ const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY || '';
 const GITHUB_BRANCH_NAME = process.env.GITHUB_HEAD_REF || '';
 const HASURA_PROJECT_DIR_PATH = process.env.HASURA_PROJECT_DIR_PATH || '';
 const PR_NUMBER = process.env.PR_NUMBER || '123';
-const CLOUD_DATA_GRAPHQL = process.env.CLOUD_DATA_GRAPHQL || 'https://data.lux-dev.hasura.me';
+const CLOUD_DATA_GRAPHQL = process.env.CLOUD_DATA_GRAPHQL_ENDPOINT || 'https://data.lux-dev.hasura.me';
+const GITHUB_OWNER = GITHUB_REPOSITORY.split('/')[0];
+const GITHUB_REPO_NAME = GITHUB_REPOSITORY.split('/')[1];
 
-const deployPreviewApp = () => {
+const createPreviewApp = () => {
+	console.log('Creating preview app');
+};
 
-	const GITHUB_OWNER = GITHUB_REPOSITORY.split('/')[0];
-	const GITHUB_REPO = GITHUB_REPOSITORY.split('/')[1];
-
-	const PREVIEW_APP_NAME = `pr-${PR_NUMBER}`;
-	console.log(
-		`
-		GITHUB_TOKEN: ${GITHUB_TOKEN},
-		GITHUB_REPO: ${GITHUB_REPO},
-		GITHUB_OWNER: ${GITHUB_OWNER},
-		GITHUB_BRANCH_NAME: ${GITHUB_BRANCH_NAME},
-		HASURA_PROJECT_DIR_PATH: ${HASURA_PROJECT_DIR_PATH},
-		HASURA_CLOUD_PAT: ${HASURA_CLOUD_PAT},
-		PREVIEW_APP_NAME: ${PREVIEW_APP_NAME},
-		CLOUD_DATA_GRAPHQL: ${CLOUD_DATA_GRAPHQL}
-		`
-	);
-
-	// const previewAppName = `pr-${PR_NUMBER}`;
-	// return fetch(
-	// 	CLOUD_DATA_GRAPHQL,
-	// 	{
-	// 		method: 'POST',
-	// 		headers: {
-	// 			'authorization': `PAT ${HASURA_CLOUD_PAT}`
-	// 		},
-	// 		body: JSON.stringify({
-	// 			query: `
-
-	// 			`,
-	// 			variables: {
-
-	// 			}
-	// 		})
-	// 	}
-	// )
+const recreatePreviewApp = () => {
+	console.log('Recreating preview app');
 }
 
-deployPreviewApp();
+const handlePREvent = () => {
+
+	const previewAppName = `${GITHUB_REPO_NAME}-pr-${PR_NUMBER}-`;
+	return fetch(
+		CLOUD_DATA_GRAPHQL_ENDPOINT,
+		{
+			method: 'POST',
+			headers: {
+				'authorization': `PAT ${HASURA_CLOUD_PAT}`
+			},
+			body: JSON.stringify({
+				query: `
+					query {
+						projects (where: { name: { _eq: "${previewAppName}"}}) {
+							id
+						}
+					}
+				`,
+			})
+		}
+	).then(r => r.json())
+	.then(response => {
+		if (response.errors) {
+			console.error('Error querying the projects');
+			process.exit(1);
+		}
+		if (response.data && response.data.projects) {
+			console.log(response);
+			if (response.data.projects.length) {
+				recreatePreviewApp();
+			} else {
+				createPreviewApp();
+			}
+		} else {
+			console.error('Unexpected response while checking project')
+			process.exit(1);
+		}
+	}).catch(e => {
+		console.log('Unexpected error');
+		console.log(e);
+		process.exit(1);
+	})
+
+}
+
+handlePREvent();
